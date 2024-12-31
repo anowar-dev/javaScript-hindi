b3dbeec5ff48c10ae5c4f0fca6e8659b19b58d0e
Md. Anower Hossan <111055028+anowar-dev@users.noreply.github.com>
2024-12-31 19:24:58 +0000

    fiter methjod

---
+++ loops/iterate.js
@@ -0,0 +1,25 @@
+
+let num = 11;
+// while (num <= 3) {
+//     console.log(num)  
+//     num ++ 
+// }
+// do {
+//     console.log(num)
+//     num ++
+// } while (num <=5);
+
+// let mayName = new Map()
+// mayName.set("ame", "anower")
+// mayName.set("name", "anowe")
+// mayName.set("names", "anower hossan")
+// for (const [k, val] of mayName) {
+//     console.log(val);
+    
+// }
+
+const numbe = [1, 2, 3, 4]
+
+numbe.forEach((v, i, a) => {
+    console.log(`the value of ${v} and index ${i} heres whole array ${a}`)
+})
